634973160832c0f04749e04c98a5c9503329a656
sumit saini <sainisumit935@yahoo.com>
2018-07-20 01:04:29 +0530

    Text Mesh over Sprites using Sorting Layer and New font added

---
+++ Assets/Scripts/SortingLayerExposer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SortingLayerExposer : MonoBehaviour {
+
+    // Use this for initialization
+    public string SortingLayerName = "Default";
+    public int SortingOrder = 0;
+
+    void Awake()
+    {
+        gameObject.GetComponent<MeshRenderer>().sortingLayerName = SortingLayerName;
+        gameObject.GetComponent<MeshRenderer>().sortingOrder = SortingOrder;
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
